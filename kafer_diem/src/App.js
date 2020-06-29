82f2a19cab491b55e39ddcba941e7b77fde9290b
mnorty <mnorty@gmail.com>
2020-06-29 16:12:08 -0600

    test

--- kafer_diem/src/App.js
+++ kafer_diem/src/App.js
@@ -7,8 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Yo</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Edit update
         </p>
         <a
           className="App-link"
